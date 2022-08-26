102cd03515c55f0460025c8f5da6e49feb8bff54
Ben een <53089901+Prutserdt@users.noreply.github.com>
2022-08-26 18:53:40 +0200

    Update konijnenbouten.php
    Onnodige tekst weggehaald

--- konijnenbouten.php
+++ konijnenbouten.php
@@ -8,7 +8,5 @@
 // Voor de statistiek heb ik deze weggehaald, anders zijn er alleen 0 en 1 scores.
 // De array nummering is geautomatiseerd met $aantalbunnies, daardoor kun je deze
 // regels eenvoudig uncommenten en daarna werkt de code gewoon.
-// In de vraag staat dat er 10-15 verschillende konijnen worden gevraagd, in het voorbeeld antwoord
-// staan er minder.
                     "(o^o)", "(-.-)", "(~.~)");
 ?>
